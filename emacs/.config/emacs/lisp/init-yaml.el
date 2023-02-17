8e4248f2803bda903e94c5a259318e5826201fef
Sebastian Burk <sebastian.burk@gmail.com>
2023-02-17 14:17:30 +0100

    added init-yaml.el

---
+++ emacs/.config/emacs/lisp/init-yaml.el
@@ -0,0 +1,11 @@
+;;; init-yaml.el --- TEXT -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package yaml-mode
+  :ensure t
+  :mode ("\\.yml\\.yaml\\'" . yaml-mode))
+
+
+(provide 'init-yaml)
+;;; init-yaml.el ends here
